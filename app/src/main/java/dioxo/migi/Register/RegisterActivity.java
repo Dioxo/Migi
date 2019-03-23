24f34c96dbcd20c1a834bb31b08a6104490799a8
Dioxo <amolinares19@gmail.com>
2019-03-23 15:45:34 +0100

    Creation activity register

---
+++ app/src/main/java/dioxo/migi/Register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package dioxo.migi.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+import dioxo.migi.R;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
