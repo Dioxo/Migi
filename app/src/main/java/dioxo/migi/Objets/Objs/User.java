cc9e9a3d3acfcc7ddd8c8a5fbfb3b7d318c9cf27
Dioxo <amolinares19@gmail.com>
2019-06-23 19:18:25 +0200

    Encriptar/desencriptar contraseñas

--- app/src/main/java/dioxo/migi/Objets/Objs/User.java
+++ app/src/main/java/dioxo/migi/Objets/Objs/User.java
@@ -3,6 +3,12 @@ package dioxo.migi.Objets.Objs;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import dioxo.migi.Objets.Objs.Seguridad.Encriptar;
+import dioxo.migi.Objets.Objs.Seguridad.Encriptar_Interface;
+
 public class User {
     @SerializedName("email")
     @Expose
@@ -14,8 +20,16 @@ public class User {
 
     public User(String email, String password, String nickName) {
         this.email = email;
-        this.password = password;
         this.nickName = nickName;
+
+        Encriptar_Interface encriptar = new Encriptar();
+        try {
+            this.password = encriptar.generateStorngPasswordHash(password);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+
     }
     public User(String email, String nickName) {
         super();
